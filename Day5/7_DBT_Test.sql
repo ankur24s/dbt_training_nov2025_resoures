444c683981527f8c17edfaff1dcf8c4f1f698122
ankur24s <ankursahu64@gmail.com>
2025-11-07 14:06:47 +0530

    Added

--- Day5/7_DBT_Test.sql
+++ Day5/7_DBT_Test.sql
@@ -6,41 +6,64 @@ select * from  dev.gold_airbnb.dim_listings
 
 select distinct room_type from  dev.gold_airbnb.dim_listings
 /*
-['Entire home/apt',
-'Private room',
-'Shared room',
-'Entire Home',
-'Hotel room']
+['Shared room',
+                      'Private room',
+                      'Vila',
+                      'Hotel room',
+                      'Entire home/apt',
+                      'Entire Home'
+                      ]
 */
 
 -- schema.yml
-version: 2
+version: 1
 
 models:
   - name: dim_listings
     columns:
-      
       - name: listing_id
         tests:
-          - unique
           - not_null
-
+          - unique:
+              config:
+                severity: warn
+                error_if: ">=5"
+                warn_if:  ">=2"
+      
+      - name: price
+        tests:
+          - positive_value
+  
+  - name: dim_hosts
+    columns:
       - name: host_id
         tests:
           - not_null
-          - relationships:
+          - unique
+
+  - name: dim_listing_w_hosts
+    columns:
+      - name: host_id
+        tests:
+          - relationships: 
               to: ref('dim_hosts')
               field: host_id
 
+      - name: minimum_nights
+        tests:
+          - positive_value
       
       - name: room_type
         tests:
           - accepted_values:
-              values: ['Entire home/apt',
+              values: ['Shared room',
                       'Private room',
-                      'Shared room',
+                      'Vila',
+                      'Hotel room',
+                      'Entire home/apt',
                       'Entire Home',
-                      'Hotel room']
+                      'Private room 123'
+                      ]
 					  
 					  
 					  
@@ -48,11 +71,24 @@ models:
 -- dbt test relationships_dim_listings_host_id__host_id__ref_dim_hosts_ (running the single test)
 						
 -- Generic tests
+
+positive_value.sql
+{% test positive_value(model, column_name) %}
+select * from {{model}} 
+where {{column_name}} < 0 
+{% endtest %}
+
+--Singular Tests
 -- create a test in test>dim_listings_minimum_nights.SQL
 select * from 
 {{ref('dim_listings')}}
 where minimum_nights <1
 limit 10
+
+--fact_reviews_min_lenght_review_text
+select length(review_text), * 
+from {{ref('fact_reviews')}}
+where  length(review_text) <4
 
 -- dbt test --select dim_listings_minimum_nights
 
